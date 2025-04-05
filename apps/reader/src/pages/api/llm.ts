7b2b10e5536b82d7b0fa800dec4248eafd37feff
shutootaki <ootaki200109@gmail.com>
2025-04-05 10:40:59 +0900

    wip

--- apps/reader/src/pages/api/llm.ts
+++ apps/reader/src/pages/api/llm.ts
@@ -2,6 +2,7 @@ import type { Document } from '@langchain/core/documents'
 import { StringOutputParser } from '@langchain/core/output_parsers'
 import { RunnablePassthrough } from '@langchain/core/runnables'
 import { RunnableSequence } from '@langchain/core/runnables'
+import { VectorStoreRetriever } from '@langchain/core/vectorstores'
 import { ChatOpenAI } from '@langchain/openai'
 import { NextApiRequest, NextApiResponse } from 'next'
 
@@ -22,27 +23,30 @@ export default async function handler(
       return res.status(400).json({ error: 'question is required' })
     }
 
-    // グローバルに保存している vector store を取得
-    // todo: 永続化
-    const vectorStore = getSharedVectorStore()
-    if (!vectorStore) {
-      console.error('Vector store is not available')
-      return res.status(500).json({ error: 'Vector store is not initialized' })
-    }
-
-    // retriever を使って、質問に関連するドキュメントを取得する
-    const vectorStoreRetriever = vectorStore.asRetriever()
-
     const model = new ChatOpenAI({
       model: 'gpt-4o-mini',
       apiKey: process.env.OPENAI_API_KEY,
     })
+    // グローバルに保存している vector store を取得
+    // todo: 永続化
+    const vectorStore = getSharedVectorStore()
+    let vectorStoreRetriever: VectorStoreRetriever
+    // if (!vectorStore) {
+    //   console.error('Vector store is not available')
+    //   return res.status(500).json({ error: 'Vector store is not initialized' })
+    // }
+    if (vectorStore) {
+      // retriever を使って、質問に関連するドキュメントを取得する
+      vectorStoreRetriever = vectorStore.asRetriever()
+    }
 
     const chain = RunnableSequence.from([
       {
-        context: vectorStoreRetriever.pipe((docs: Document[]) =>
-          formatDocumentsAsString(docs),
-        ),
+        context: vectorStoreRetriever
+          ? vectorStoreRetriever.pipe((docs: Document[]) =>
+              formatDocumentsAsString(docs),
+            )
+          : new RunnablePassthrough(),
         question: new RunnablePassthrough(),
       },
       ragPrompt,
